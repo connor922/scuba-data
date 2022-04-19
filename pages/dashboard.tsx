7e85b0b9e6fc6120ecad338a65ddd6a3274eea2b
Conan Malanaphy <conanmalanaphy@gmail.com>
2022-04-19 21:51:51 +0100

    added dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,27 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Scuba Data</title>
+        <title>Dashboard</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">ScubaData!</a>
-        </h1>        
+       This is where the dashboard is!
       </main>
-
-      <footer className={styles.footer}>
-        <Link href="/dashboard">
-          <a>Go to Dashboard</a>
-        </Link>
-        </footer>
     </div>
   )
 }

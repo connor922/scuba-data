5ecd0a9f1a2b4228a025de0826c64b14a49cac27
Conan Malanaphy <conanmalanaphy@gmail.com>
2022-04-20 23:07:20 +0100

    matching styles on dashboard and settings page

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -1,37 +1,196 @@
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-import * as React from 'react';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import MenuIcon from '@mui/icons-material/Menu'
+import ScubaDivingIcon from '@mui/icons-material/ScubaDiving'
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications'
+import MuiAppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import MuiDrawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
+import List from '@mui/material/List'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Paper from '@mui/material/Paper'
+import { createTheme, styled, ThemeProvider } from '@mui/material/styles'
+import Toolbar from '@mui/material/Toolbar'
+import { Fragment, useState } from 'react'
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 
-const Home: NextPage = () => {
-  const [expanded, setExpanded] = React.useState("");
+const drawerWidth = 240;
 
+const AppBar:any = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }:any) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
+
+
+export const secondaryListItems = (
+  <Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+      <Link href="/dashboard" >
+              <DashboardIcon />
+              </Link>
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+      <Link href="/settings" >
+              <SettingsApplicationsIcon />
+              </Link>
+      </ListItemIcon>
+      <ListItemText primary="Settings" />
+    </ListItemButton>
+  </Fragment>
+);
+
+const mdTheme = createTheme();
+
+function DashboardContent() {
+  const [open, setOpen] = useState(false);
+  const [expanded, setExpanded] = useState("");
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+  
   const handleChange = (panel:any) => (event:any, isExpanded:any) => {
     setExpanded(isExpanded ? panel : false);
   };
 
-
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Dashboard</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+               <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1, color: '#FFFFFF' }}
+            >
+               <IconButton color="inherit" sx={{ color: '#FFFFFF' }}>
+               <Link href="/" sx={{ color: '#FFFFFF' }}>
+              <ScubaDivingIcon  sx={{ color: '#FFFFFF' }}/>
+              </Link>
+            </IconButton>
 
-      <main className={styles.main}>
-       <h1>This is where the settings will go</h1>
-       <Link href="/dashboard">
-         Back to Dashboard
-      </Link>
-      <div>
-      <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              px: [1],
+            }}
+          >
+            Dashboard
+            <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+          {secondaryListItems}
+            <Divider sx={{ my: 1 }} />
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '100%',
+                    width: '100%',
+                    marginTop:2
+                  }}
+                >
+                    <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <AccordionSummary
           aria-controls="panel1bh-content"
           id="panel1bh-header"
@@ -63,11 +222,15 @@ const Home: NextPage = () => {
         </AccordionDetails>
       </Accordion>
 
-    </div>
-      </main>
-      
-    </div>
-  )
+             
+       
+                </Paper>
+              
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-export default Home
+export default DashboardContent;

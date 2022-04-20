4d4c854f5f8c39773e54cb740f827efbcde6dd73
Conan Malanaphy <conanmalanaphy@gmail.com>
2022-04-20 22:36:35 +0100

    Templates for pages

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,46 +1,207 @@
-import type { NextPage } from 'next'
-import { useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-import Button from '@mui/material/Button';
-import Modal from '../components/Modal/Modal';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import MenuIcon from '@mui/icons-material/Menu'
+import ScubaDivingIcon from '@mui/icons-material/ScubaDiving'
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications'
+import MuiAppBar from '@mui/material/AppBar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import MuiDrawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import Link from '@mui/material/Link'
+import List from '@mui/material/List'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ListItemText from '@mui/material/ListItemText'
+import Paper from '@mui/material/Paper'
+import { createTheme, styled, ThemeProvider } from '@mui/material/styles'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import { Fragment, useState } from 'react'
+import Modal from '../components/Modal/Modal'
 
-const Home: NextPage = () => {
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== 'open',
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(['width', 'margin'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    '& .MuiDrawer-paper': {
+      position: 'relative',
+      whiteSpace: 'nowrap',
+      width: drawerWidth,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      boxSizing: 'border-box',
+      ...(!open && {
+        overflowX: 'hidden',
+        transition: theme.transitions.create('width', {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        width: theme.spacing(7),
+        [theme.breakpoints.up('sm')]: {
+          width: theme.spacing(9),
+        },
+      }),
+    },
+  }),
+);
+
+
+export const secondaryListItems = (
+  <Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+      <Link href="/dashboard" >
+              <DashboardIcon />
+              </Link>
+      </ListItemIcon>
+      <ListItemText primary="Dashboard" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+      <Link href="/settings" >
+              <SettingsApplicationsIcon />
+              </Link>
+      </ListItemIcon>
+      <ListItemText primary="Settings" />
+    </ListItemButton>
+  </Fragment>
+);
+
+const mdTheme = createTheme();
+
+function DashboardContent() {
+  const [open, setOpen] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
-
   const handleClickOpen = () => {
     setIsOpen(true);
   };
-
   const handleClose = () => {
     setIsOpen(false);
   };
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Dashboard</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="absolute" open={open}>
+          <Toolbar
+            sx={{
+              pr: '24px', // keep right padding when drawer closed
+            }}
+          >
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              onClick={toggleDrawer}
+              sx={{
+                marginRight: '36px',
+                ...(open && { display: 'none' }),
+              }}
+            >
+               <MenuIcon />
+            </IconButton>
+            <Typography
+              component="h1"
+              variant="h6"
+              color="inherit"
+              noWrap
+              sx={{ flexGrow: 1, color: '#FFFFFF' }}
+            >
+               <IconButton color="inherit" sx={{ color: '#FFFFFF' }}>
+               <Link href="/" sx={{ color: '#FFFFFF' }}>
+              <ScubaDivingIcon  sx={{ color: '#FFFFFF' }}/>
+              </Link>
+            </IconButton>
 
-      <main className={styles.main}>
-       <h1>This is where the dashboard is!</h1>
-       <Button variant="outlined" onClick={handleClickOpen}>
-        Open form dialog
-      </Button>
-      <Modal isOpen={isOpen} handleClose={handleClose} />
-       <Link href="/">
-          Go to the HomePage
-      </Link>
-      <Link href="/settings">
-          Go to Settings
-      </Link>
-      </main>
-      
-    </div>
-  )
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <Toolbar
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              px: [1],
+            }}
+          >
+            Dashboard
+            <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+            </IconButton>
+          </Toolbar>
+          <Divider />
+          <List component="nav">
+          {secondaryListItems}
+            <Divider sx={{ my: 1 }} />
+          </List>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Button variant="outlined" onClick={handleClickOpen}>
+                      Open form dialog
+                    </Button>
+                    <Modal isOpen={isOpen} handleClose={handleClose} />
+             
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '100%',
+                    width: '100%',
+                    marginTop:2
+                  }}
+                >
+                    
+       
+                </Paper>
+              
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-export default Home
+export default DashboardContent;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,102 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Link from 'next/link'
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const Home: NextPage = () => {
+const theme = createTheme();
+
+export default function SignInSide() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
   return (
-    <Container maxWidth="sm">
-       <Head>
-        <title>Scuba Data</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    <Box sx={{ my: 4 }}>
-      <Typography variant="h1" component="h1" gutterBottom>
-        Welcome to ScubaData
-      </Typography>
-      <Link href="/dashboard">
-          Go to the Dashboard
-      </Link>
-    </Box>
-  </Container>
-  )
+    <ThemeProvider theme={theme}>
+      <Grid container component="main" sx={{ height: '100vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: 'url(/scuba.jpg)',
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+          }}
+        />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: '#1976d2' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+                <Button
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  <Link href="/dashboard" sx={{ color: '#FFFFFF' }} >
+                      Sign In
+                  </Link>
+                </Button>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
+  );
 }
-
-export default Home

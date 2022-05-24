e86ee99a8563373abb70cdc9b0efa70b8fcd9fe9
Conan Malanaphy <conanmalanaphy@gmail.com>
2022-05-24 20:31:14 +0100

    add files

---
+++ pages/archieved-campaigns.tsx
@@ -0,0 +1,96 @@
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import CssBaseline from '@mui/material/CssBaseline'
+import Paper from '@mui/material/Paper'
+import Toolbar from '@mui/material/Toolbar'
+import { useEffect, useState } from 'react'
+import CampaignModal from '../components/CampaignModal/CampaignModal'
+import Wrapper from '../components/Wrapper/Wrapper'
+import Accord from '../components/Accord/Accord'
+import CircularProgress from '@mui/material/CircularProgress';
+
+import useSWR from "swr";
+
+const fetcher = async (url: string) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw Error("Yo that's NOT OK!!!");
+  }
+  const data = await res.json();
+  return data;
+};
+
+function Settings() {
+    const [expanded, setExpanded] = useState('')
+    const [state, setState] = useState<any[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const result = useSWR(`/api/user/1`, fetcher);
+    const data = result.data;
+   
+    const handleChange = (event: any) => {
+        setExpanded((prevstate: any) => {
+            return event == prevstate ? '' : event
+        })
+    }
+
+    useEffect(()=>{
+        if(data){
+            setState(data.filter((a:any) => a.state === "ARCHIVED"));
+            setIsLoading(false);
+        }
+    },[data])
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            
+            <CssBaseline />
+            <Wrapper pageName={'Settings'} />
+            <Box
+                sx={{
+                    backgroundColor: (theme) =>
+                        theme.palette.mode === 'light'
+                            ? theme.palette.grey[100]
+                            : theme.palette.grey[900],
+                    flexGrow: 1,
+                    height: '100vh',
+                    overflow: 'auto',
+                }}
+            >
+                <Toolbar />
+                <Box sx={{ m: 4 }}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            height: '100%',
+                            width: '100%',
+                            marginTop: 2,
+                            gap: '2rem',
+                        }}
+                    > 
+                    {!isLoading ? state.map((item: any) => {
+                            return (
+                                <Accord
+                                    key={item.id}
+                                    setState={setState}
+                                    seniorites={item.seniorites}
+                                    keywords={item.keywords}
+                                    companysList={item.companysList}
+                                    jobTitles={item.jobTitles}
+                                    isExpanded={expanded == item.id.toString()}
+                                    id={item.id}
+                                    handleChange={handleChange}
+                                    name={item.name}
+                                    state={item.state}
+                                />
+                            )}):  <Box sx={{ display: 'flex', width:"100%", justifyContent: 'center' }}>
+                            <CircularProgress />
+                          </Box>}
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+export default Settings
